469d99f7416258996e2594f1b0e5749d554bf682
Rory Comiskey <rorycomiskey@hotmail.com>
2017-11-24 12:32:37 +0000

    created blog app

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+   class Meta:
+       model = Post
+       fields = ( 'title', 'content', 'image',)
+       
+class CommentsForm(forms.ModelForm):
+   class Meta:
+       model = Post
+       fields = ( 'title', 'content')       
