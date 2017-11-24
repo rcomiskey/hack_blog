a6ad15db6201fdaed890e7ae1aefe4cadd8c61f2
Stephen Scott <stephenjmscott@gmail.com>
2017-11-24 13:04:24 +0000

    Added Password Reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 #ACCOUNTS URLS#
 
 from django.conf.urls import url, include
-
 from .views import logout, login, register, profile
+from . import urls_reset
 
 urlpatterns = [
     url(r'^logout', logout, name="logout"),
     url(r'^login', login, name="login"),
     url(r'^register', register, name="register"),
     url(r'^profile', profile, name="profile"),
+    url(r'^password-reset/', include(urls_reset)),
     ]
--- hack_blog/.~c9_invoke_YmE82Z.py
+++ hack_blog/.~c9_invoke_YmE82Z.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'blog',
     'django_forms_bootstrap',
 ]
 
@@ -67,7 +66,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,16 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+Des Cahill [12:37 PM] 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+  os.path.join(BASE_DIR, "static"),
 )
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'accounts.backends.EmailAuth'
-]
